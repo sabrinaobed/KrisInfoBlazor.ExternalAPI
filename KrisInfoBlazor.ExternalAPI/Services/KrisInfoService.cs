b76cfa42e2525d86a2f3d10033b5a82c06f8c334
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-04-15 19:28:42 +0200

    Added services through DI and then added index.razor page

--- KrisInfoBlazor.ExternalAPI/Services/KrisInfoService.cs
+++ KrisInfoBlazor.ExternalAPI/Services/KrisInfoService.cs
@@ -1,6 +1,28 @@
-﻿namespace KrisInfoBlazor.ExternalAPI.Services
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using KrisInfoBlazor.ExternalAPI.Models;
+
+
+
+
+namespace KrisInfoBlazor.ExternalAPI.Services
 {
     public class KrisInfoService
     {
+        private readonly HttpClient _http;
+
+        public KrisInfoService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<NewsItem>> GetNewsAsync()
+        {
+            var response = await _http.GetStringAsync("https://api.krisinformation.se/v3/news");
+            var items = JsonConvert.DeserializeObject<List<NewsItem>>(response);
+            return items ?? new List<NewsItem>();
+        }
     }
 }
