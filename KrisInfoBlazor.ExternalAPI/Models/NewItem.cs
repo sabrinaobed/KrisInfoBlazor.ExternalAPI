acaea937e0898de17c636596ca8faced9f882404
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-04-15 19:04:19 +0200

    Created a class by name NewItem and added properties

---
+++ KrisInfoBlazor.ExternalAPI/Models/NewItem.cs
@@ -0,0 +1,11 @@
+﻿namespace KrisInfoBlazor.ExternalAPI.Models
+{
+    public class NewsItem
+    {
+        public string Identifier { get; set; }
+        public string Headline { get; set; }
+        public string Preamble { get; set; }
+        public string BodyText { get; set; }
+        public DateTime Published { get; set; }
+    }
+}
